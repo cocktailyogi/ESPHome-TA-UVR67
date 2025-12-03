74a9bcd91e91222c9171a0b2e46227ccb3b11c50
cocktailyogi <cocktail_yogi@web.de>
2025-12-03 12:45:46 +0100

    acaec

--- components/sensordlbus/DLBus.cpp
+++ components/sensordlbus/DLBus.cpp
@@ -249,13 +249,13 @@ void DLBus::sensorSlaveRespond(byte sensorAddress){
         DLBus::sendManchesterByte(DatenbyteLow);
         DLBus::sendManchesterByte(DatenbyteHigh);
         DLBus::sendManchesterByte(checksum);
+        //disable TX-Output
+        digitalWrite(DL_Output_Pin, LOW);
         ESP_LOGI(TAG, "sensorAddress=0x%02X", sensorAddress);
         ESP_LOGI(TAG, "Datenkennzeichnung=0x%02X", Datenkennzeichnung);
         ESP_LOGI(TAG, "DatenbyteLow=0x%02X", DatenbyteLow);
         ESP_LOGI(TAG, "DatenbyteHigh=0x%02X", DatenbyteHigh);
         ESP_LOGI(TAG, "checksum=0x%02X", checksum);
-        //disable TX-Output
-        digitalWrite(DL_Output_Pin, LOW);
         //send slave-Response
         ESP_LOGI(TAG, "RAS-PT request processed");
     }
